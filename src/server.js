7767c0329d512ab0989bc95cf399a2034d630281
JonathanCantu <jcantu@jcantu.com>
2025-02-13 16:07:41 -0700

    Add Pagination and include preliminary Typescript

--- src/server.js
+++ src/server.js
@@ -19,12 +19,26 @@ const pool = new Pool({
 });
 
 app.get('/api/data', async (req, res) => {
+  const limit=parseInt(req.query['limit']);
+  const offset=parseInt(req.query['offset']);
+  const search=(req.query['search']);
   try {
-    const result = await pool.query('SELECT * FROM elements_elements');
+    let result;
+
+    if (search) {
+      result = await pool.query(`SELECT * FROM elements_elements WHERE element ILIKE '%${search}%'`);
+    } else {
+      result = await pool.query(`SELECT * FROM elements_elements LIMIT ${limit} OFFSET ${offset}`);
+    }
+
     res.json(result.rows);
+
   } catch (err) {
+
     console.error(err.message);
+
     res.status(500).send('Server Error');
+
   }
 });
 
